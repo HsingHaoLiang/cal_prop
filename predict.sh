1c25c99b57ce9e63716099d34c9dbba84b7468b3
HsingHaoLiang <xerion07041@gmail.com>
2026-02-05 19:15:49 +0800

    Update predict.sh

--- predict.sh
+++ predict.sh
@@ -1,10 +1,11 @@
 python predict.py \
   --xlsx-path input_features.xlsx \
-  --smiles-csv example.csv --smiles-col smiles --tr-col "T(K)" \
+  --input-csv example.csv --smiles-col smiles --tr-col "T(K)" \
   --model-dir ./model_save \
   --stats-json normalization_stats.json \
   --rep FP \
   --task both_real \
   --i-list 1-10 \
   --j-list 1-10 \
-  --out-csv output.csv
+  --out-csv output.csv \
+  --progress
